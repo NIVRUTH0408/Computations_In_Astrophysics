8006d8812758ce88b9898b4c7de79fe05f430979
Nivruth Narayan  V M <nivruth0408@gmail.com>
2026-06-10 18:58:50 +0530

    Update freefall_eulers
    Added a threshold for distance to stop calculations when reaching the radius of the celestial body.

--- freefall/freefall_eulers.m
+++ freefall/freefall_eulers.m
@@ -1,17 +1,18 @@
 r0 = 1.5e11;
 M = 2e30;
 G = 6.67430e-11; 
+R = 6.957e8;
 v0 = 0;
-dt = 100000;
-t_eulers = 0:dt:1e8;
+dt_eulers = 100000;
+t_eulers = 0:dt_eulers:1e8;
 r_eulers = zeros(size(t_eulers));
 v_eulers = zeros(size(t_eulers));
 r_eulers(1) = r0; 
 v_eulers(1) = v0; 
 for i=1:length(t_eulers)-1
-    r_eulers(i+1) = r_eulers(i)+(dt*v_eulers(i));
-    v_eulers(i+1) = v_eulers(i) - (G * M / r_eulers(i)^2) * dt;
-    if r_eulers(i+1)<=0
+    r_eulers(i+1) = r_eulers(i)+(dt_eulers*v_eulers(i));
+    v_eulers(i+1) = v_eulers(i) - (G * M / r_eulers(i)^2) * dt_eulers;
+    if r_eulers(i+1)<=R
         t_eulers = t_eulers(1:i+1);
         r_eulers = r_eulers(1:i+1);
         v_eulers = v_eulers(1:i+1);
@@ -31,3 +32,4 @@ plot(t_eulers, v_eulers,'r');
 xlabel('Time (s)');
 ylabel('Velocity (m/s)');
 title('Velocity vs Time');
+sgtitle('Free Fall-Eulers Method')
